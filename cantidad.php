f3892a5b892168a09adaf8efc3702aac3e2e6894
marc98sjd <mguerraexposito@iesesteveterradas.cat>
2017-10-17 20:18:59 +0200

    afegit flip, css i carta por detras

--- cantidad.php
+++ cantidad.php
@@ -7,61 +7,85 @@
       $columnas_pares=0;
     ?>
     <style type="text/css">
-      td{
-        padding: 20px;
-      }
-      .naranja{
-        background-color: orange;
-      }
-      .amarilla{
-        background-color: yellow;
-      }
       body{
-        background-color: #56c9b2;
+        background-image: url("fondo_pagina.jpg");
+        background-repeat: no-repeat;
       }
       table{
         margin-right: auto;
         margin-left:  auto;
         width: 65%;
         height: 65%;
-        margin-top: 12%;
+        border: hidden;
+        border-collapse: collapse;
+      }
+      td{
+        padding: 5px;
+        border:none;
+      }
+      /* entire container, keeps perspective */
+      .flip-container {
+        perspective: 1000px;
+       }
+      /* flip the pane when hovered */
+      .flip-container:hover .flipper, .flip-container.hover .flipper {
+        transform: rotateY(180deg);
+      }
+      .flip-container, .front, .back {
+        width: 100px;
+        height: 150px; 
+      }
+      /* flip speed goes here */
+      .flipper {
+        transition: 1.0s;
+        transform-style: preserve-3d;
+        position: relative;
+      }
+      /* hide back of pane during swap */
+      .front, .back {
+        backface-visibility: hidden;
+        position: absolute;
+        top: 0;
+        left: 0;
+      }
+      /* front pane, placed above back */
+      .front {
+        z-index: 2;
+        /* for firefox 31 */
+        transform: rotateY(0deg);
+      }
+      /* back, initially hidden pane */
+      .back {
+        transform: rotateY(180deg);
+      } 
+      .fotos{
+        width: 100%;
+        height: 100%; 
+        border-radius: 20px;
       }
     </style>                                         
   </head>                                                                 
-  <body>      
+  <body>
     <table border="15">
       <?php
-        for($i=0;$i<$filas;$i++){
+        for($i=0;$i<$filas;$i++){ 
           echo "<tr>";
-          $contador=$contador+1;
-          if($contador%2==0){
             for($x=0;$x<$columnas;$x++){
-              if($columnas_pares%2==0){
-                $columnas_pares=$columnas_pares+1;
-                echo "<td class='naranja'>  </td>";
-              }
-              else{
-                $columnas_pares=$columnas_pares+1;
-                echo "<td class='amarilla'></td>";
-              }
+              echo "<td class='color_fondo'>
+                      <div class='flip-container'>
+                          <div class='flipper'>
+                            <div class='front'>
+                              <img src='tras_carta.jpg' class='fotos'>
+                            </div>
+                            <div class='back'>
+                              <img src='vaca.jpg' class='fotos'>
+                            </div>
+                          </div>
+                      </div>
+                    </td>";
             }
-          }
-          else{
-            for($x=0;$x<$columnas;$x++){
-              if($columnas_pares%2==0){
-                $columnas_pares=$columnas_pares+1;
-                echo "<td class='amarilla'></td>";
-                $contador=$contador+1;
-              }
-              else{
-                $columnas_pares=$columnas_pares+1;
-                echo "<td class='naranja'></td>";
-                $contador=$contador+1;
-              }
-            }
-          }
           echo "</tr>";
-        }
+        } 
       ?>
     </table>
   </body>                                                                 
